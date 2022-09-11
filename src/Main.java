abade2a2556ecb721e29681dcf4e9835c04525b3
massagist2010 <rusy_boob2@mail.ru>
2022-09-11 20:32:40 +0300

    H.W.2 д.з.2\ 4 и 5

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        //дом.зад.2 задача 4
+        System.out.println();
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        //дом.зад.2\5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = 10 +4;
+        System.out.println(frog);
+    }
+}
